d8dbddedbe819b6a9e85d83ef708a1be2f002208
OrthodoxAll <aleksandrnagiev@yandex.ru>
2026-08-12 23:18:34 +0700

    add funcs sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -23,3 +23,17 @@ print(filter_by_state([{'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-0
  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],'CANCELED'))
 
 
+def sort_by_date(data: list[dict], descending:bool =True) -> list[dict]:
+    """
+    принимает список словарей и необязательный параметр,
+    задающий порядок сортировки (по умолчанию — убывание).
+    Функция должна возвращать новый список, отсортированный по дате
+    (date).
+    :param data: [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
+    :param descending: True по умолчанию убывание , от свежей даты к старшей
+    """
+    return sorted(data, key= lambda x: x.get('date'), reverse=descending)
+
+print(sort_by_date([{'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+ {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+ {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], False))
