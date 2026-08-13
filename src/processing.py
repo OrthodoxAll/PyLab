96e2244026a75c1c009aafb025a55efa51452b23
OrthodoxAll <aleksandrnagiev@yandex.ru>
2026-08-13 20:06:16 +0700

    chect linters

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 
-def filter_by_state(data: list[dict[str,Any]], state: str = "EXECUTED") -> list[dict[str,Any]]:
+def filter_by_state(data: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
     """
         принимает список словарей и опционально значение для ключа
     state
@@ -18,7 +18,7 @@ def filter_by_state(data: list[dict[str,Any]], state: str = "EXECUTED") -> list[
     return [item for item in data if item.get("state") == state]
 
 
-def sort_by_date(data: list[dict[str,Any]], descending: bool = True) -> list[dict[str,Any]]:
+def sort_by_date(data: list[dict[str, Any]], descending: bool = True) -> list[dict[str, Any]]:
     """
     принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — убывание).
@@ -27,5 +27,5 @@ def sort_by_date(data: list[dict[str,Any]], descending: bool = True) -> list[dic
     :param data: [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
     :param descending: True по умолчанию убывание , от свежей даты к старшей
     """
-    data_filtred=filter(lambda item: item.get("date") != None, data)
-    return list(sorted(data_filtred, key=lambda x: x["date"], reverse=descending))  # сортировка data
+    data_filtered = filter(lambda item: item.get("date") is not None, data)
+    return list(sorted(data_filtered, key=lambda x: x["date"], reverse=descending))  # сортировка data
