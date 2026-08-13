60ae736f69ec142e2219e2ac48841d4276f0d4c2
OrthodoxAll <aleksandrnagiev@yandex.ru>
2026-08-13 20:02:49 +0700

    fixed sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,7 @@
-def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
+from typing import Any
+
+
+def filter_by_state(data: list[dict[str,Any]], state: str = "EXECUTED") -> list[dict[str,Any]]:
     """
         принимает список словарей и опционально значение для ключа
     state
@@ -11,15 +14,11 @@ def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
         :param state: 'EXECUTED'
     """
-    # filtered_items=[] # создаем пустой список
-    # for item in data: # проходим по словаряь
-    #     if item.get('state') == state: # проверяем совпадение со state
-    #         filtered_items.append(item)
-    # return filtered_items
+
     return [item for item in data if item.get("state") == state]
 
 
-def sort_by_date(data: list[dict], descending: bool = True) -> list[dict]:
+def sort_by_date(data: list[dict[str,Any]], descending: bool = True) -> list[dict[str,Any]]:
     """
     принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — убывание).
@@ -28,4 +27,5 @@ def sort_by_date(data: list[dict], descending: bool = True) -> list[dict]:
     :param data: [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
     :param descending: True по умолчанию убывание , от свежей даты к старшей
     """
-    return sorted(data, key=lambda x: x.get("date"), reverse=descending)
+    data_filtred=filter(lambda item: item.get("date") != None, data)
+    return list(sorted(data_filtred, key=lambda x: x["date"], reverse=descending))  # сортировка data
