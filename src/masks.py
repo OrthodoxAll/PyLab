3158c21f2d298bddc509a16976a3dfd39486b490
OrthodoxAll <aleksandrnagiev@yandex.ru>
2026-09-26 06:53:30 +0700

    upddate Logger set level debug

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,9 @@
 import logging
 
 logger = logging.getLogger("masks")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 file_handler = logging.FileHandler("../logs/masks.log", mode="w", encoding="utf-8")
-file_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.DEBUG)
 file_formater = logging.Formatter("%(asctime)s - %(name)s -" " %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
@@ -21,7 +21,7 @@ def get_mask_card_number(card_number: str) -> str:
         print("Проверьте ввод, Номер карты должен состоять из 16 цифр.")
         raise ValueError("Номер карты должен состоять из 16 цифр.")
     masked_number = card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[12:]
-    logger.info(f"Карта {card_number} успешно замаскирована")
+    logger.debug(f"Карта {card_number} успешно замаскирована")
     return masked_number
 
 
@@ -36,7 +36,7 @@ def get_mask_account(account_number: str) -> str:
         print("Проверьте ввод. Аккаунт должен состоять из 20 цифр.")
         raise ValueError("Номер аккаунта должен состоять из 20 цифр.")
     mask_account = "**" + account_number[16:]
-    logger.info(f"Карта {account_number} успешно замаскирована")
+    logger.debug(f"Карта {account_number} успешно замаскирована")
     return mask_account
 
 
--- src/utils.py
+++ src/utils.py
@@ -3,9 +3,9 @@ import logging
 from os import name
 
 logger = logging.getLogger('utils')
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 file_handler = logging.FileHandler("../logs/utils.log", mode='w', encoding='utf-8')
-file_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.DEBUG)
 file_formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
@@ -18,12 +18,12 @@ def load_transactions(path: str) -> list[dict]:
     :param path:  Путь до JSON-файла
     """
     try:
-        logger.info(f'Запгрузка транзакций из {path}')
+        logger.debug(f'Запгрузка транзакций из {path}')
         with open(path, "r", encoding="utf-8") as f:
             transactions = json.load(f)
             # print(f"Загруженные данные: {transactions}")  # Отладочная информация
             if isinstance(transactions, list):
-                logger.info(f"Успешно загружены транзакции из файла {path}")
+                logger.debug(f"Успешно загружены транзакции из файла {path}")
                 return transactions
             else:
                 logger.error(f'Файл {path} не содержит транзапкций')
